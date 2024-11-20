d6b61131906374eacc6e95ad86fc84c9f51f40ab
Hex <hex@hex.ooo>
2024-11-20 23:39:07 +0000

    "Production" -> "live", "current lines" -> "output state", redo log colors, use Temporal for timestamps, pass actual bot names around

--- src/app.mjs
+++ src/app.mjs
@@ -5,26 +5,51 @@ import everyTmnt from './bots/every-tmnt/index.js'
 import howlBot from './bots/howl-bot/index.js'
 
 import { post } from './common/bluesky.js'
-import { errorQuit, timestamp } from './common/utils.js'
+import { errorQuit, getCurrentMinute, timestamp } from './common/utils.js'
 
 import { ONE_HOUR, ONE_SECOND } from './common/constants.js'
 
 const argv = minimist(process.argv.slice(2))
 
-// TODO: Configuration file
 const bots = {
-	danceBandsBot, everyTmnt, howlBot
+	'dance-bands-bot': {
+		generator: danceBandsBot
+	},
+	'every-tmnt': {
+		generator: everyTmnt
+	},
+	'howl-bot': {
+		generator: howlBot,
+		type: 'recital'
+	}
 }
 
-const mode = argv.production ? 'production' : 'demo'
+const demo = argv.live === undefined
 
 for (let bot in bots) {
+	if (bots[bot].type === 'recital') {
+		recitalBotFirstRun()
+	}
+
 	setInterval(
 		() => post({
 			bot,
-			text: bots[bot]({ mode }),
-			mode
+			demo,
+			text: bots[bot].generator({ demo })
 		}),
-		mode === 'production' ? ONE_HOUR : ONE_SECOND * 2
+		demo ? ONE_SECOND * 2 : ONE_HOUR
 	)
+}
+
+function recitalBotFirstRun () {
+// 	const currentMinute = getCurrentMinute()
+// 	const statePath = get bot state path here
+// 	const outputState = getOutputState(statePath)
+// 	const outputMinute = outputState[mode].minute
+//
+// 	if !outputMinute set it to currentMinute - how? setOutputState I guess
+//
+// 	if outputMinute === currentMinute recite()
+// 	else setTimeout to go next time the minute comes - calculate how long
+// 	*then* setInterval
 }
--- src/common/utils.js
+++ src/common/utils.js
@@ -1,13 +1,23 @@
+import { readFileSync, writeFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
 import chalk from 'chalk'
+import { Temporal } from 'temporal-polyfill'
 
 const padBotName = botName => botName.padEnd(15, ' ')
 
-export function logDemo ({ bot, text }) {
-	console.info(`${chalk.yellow('[Demo:')} ${chalk.cyan(padBotName(bot))}${chalk.yellow(']')} ${text}`)
-}
+export function logPost ({ bot, demo, text }) {
+	let logEntry = demo
+		? chalk.red('[Demo: ')
+		: chalk.hex('#FFCC33')('[')
+	logEntry += chalk.hex('#FF9900').bold(padBotName(bot))
+	logEntry += ' ' + timestamp()
+	logEntry += demo
+		? chalk.red('] ')
+		: chalk.hex('#FFCC33')('] ')
+	logEntry += text
 
-export function logPost ({ bot, text }) {
-	console.info(`${chalk.green('[' + padBotName(bot) + ']')} [${timestamp()}] ${text}`)
+	console.info(logEntry)
 }
 
 export function errorQuit (message) {
@@ -20,8 +30,50 @@ export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 export function timestamp () {
 	const pad = num => num.toString().padStart(2, '0')
 
-	const date = new Date()
+	return Temporal.Now.instant().toLocaleString('en-GB', {
+		timeZone: 'UTC',
+		timeZoneName: 'short',
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	})
+}
 
-	return `${pad(date.getUTCDate())}/${pad(date.getUTCMonth() + 1)}/${date.getUTCFullYear()} `
-		+ `${pad(date.getUTCHours())}:${pad(date.getUTCMinutes())}:${pad(date.getUTCSeconds())} UTC`
+export const getCurrentMinute = () => Temporal.Now.instant().minute
+
+export function getOutputState (path) {
+	const minute = getCurrentMinute()
+
+	let outputState = {
+		demo: {
+			line: null,
+			minute
+		},
+		live: {
+			line: null,
+			minute
+		}
+	}
+
+	try {
+		outputState = JSON.parse(
+			readFileSync(
+				resolve(path),
+				{ encoding: 'utf8' }
+			)
+		)
+	} catch (error) {
+		if (error.code !== 'ENOENT') errorQuit(error.message)
+	}
+
+	return outputState
+}
+
+export function setOutputState ({ outputState, path }) {
+	writeFileSync(path, JSON.stringify(outputState), 'utf8', error => {
+		if (error) errorQuit(error.message)
+	})
 }

80fc083caed3af16c1846cad7c30d191271f5ab3
Hex <hex@hex.ooo>
2024-12-14 00:01:58 +0000

    Include possessives in definition of boring output requiring band names to be generated again

--- src/bots/dance-bands-bot/index.js
+++ src/bots/dance-bands-bot/index.js
@@ -5,7 +5,7 @@ const recipeNames = await loadRecipes('dance-bands-bot')
 export default function generate () {
 	const out = makeRecipe(recipeNames.BAND_NAME)
 
-	const boring = /^(?:\w+ ){1,2}\w+$/
+	const boring = /^(?:[\w']+ ){1,2}\w+$/
 
 	// Pobody's nerfect
 	if (boring.test(out)) return generate()
